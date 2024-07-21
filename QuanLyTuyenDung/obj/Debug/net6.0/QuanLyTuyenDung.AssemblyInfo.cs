f88eabc26515c0bcbd4840aeecbd384f51a89083
anhduc2204 <101176957+anhduc2204@users.noreply.github.com>
2024-07-22 02:01:30 +0700

    log

--- QuanLyTuyenDung/obj/Debug/net6.0/QuanLyTuyenDung.AssemblyInfo.cs
+++ QuanLyTuyenDung/obj/Debug/net6.0/QuanLyTuyenDung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyTuyenDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908d1efd117a38f2d6c92981c5698200d61aee23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881a68a896b239d0467153313616bf5fb604de06")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyTuyenDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyTuyenDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

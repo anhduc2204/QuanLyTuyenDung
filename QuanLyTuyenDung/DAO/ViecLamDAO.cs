0469adc993c076e4dd944e9ede64b698c5008732
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2024-07-24 00:08:58 +0700

    log

--- QuanLyTuyenDung/DAO/ViecLamDAO.cs
+++ QuanLyTuyenDung/DAO/ViecLamDAO.cs
@@ -1,7 +1,36 @@
-﻿namespace QuanLyTuyenDung.DAO
+﻿using QuanLyTuyenDung.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuanLyTuyenDung.DAO
 {
 	public class ViecLamDAO
 	{
+        private readonly DataContext _dataContext;
 
-	}
+        public ViecLamDAO(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<List<ViecLam>> GetAll()
+        {
+            return await _dataContext.DSViecLam
+                .Include(vl => vl.DSDonUT)
+                .ToListAsync<ViecLam>();
+        }
+
+        public async Task<ViecLam> Save(ViecLam viecLam)
+        {
+            var vl = await _dataContext.DSViecLam.AddAsync(viecLam);
+            await _dataContext.SaveChangesAsync();
+            return vl.Entity;
+        }
+
+        public async Task<ViecLam> GetByID(int id)
+        {
+            return await _dataContext.DSViecLam.FindAsync(id);
+
+        }
+    }
 }
+ 
--- QuanLyTuyenDung/Program.cs
+++ QuanLyTuyenDung/Program.cs
@@ -1,4 +1,6 @@
-﻿using QuanLyTuyenDung.DAO;
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyTuyenDung.DAO;
+using QuanLyTuyenDung.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
-
+builder.Services.AddDbContext<DataContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddScoped<ViecLamDAO>(); //Đăng kí DAO
 builder.Services.AddScoped<NguoiDungDAO>();

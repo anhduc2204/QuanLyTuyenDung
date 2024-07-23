e8e4ec85ba8372b7b62cf5e90c24f85c91ea0afe
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2024-07-23 18:22:38 +0700

    log

--- QuanLyTuyenDung/Controllers/UngTuyenController.cs
+++ QuanLyTuyenDung/Controllers/UngTuyenController.cs
@@ -2,5 +2,26 @@
 {
 	public class UngTuyenController
 	{
-	}
+        private readonly ViecLamDAO _viecLamDAO;
+        private readonly NguoiDungDAO _nguoiDungDAO;
+        private readonly UngTuyenDAO _ungTuyenDAO;
+
+        public AdminController(ViecLamDAO viecLamDAO, NguoiDungDAO nguoiDungDAO, UngTuyenDAO ungTuyenDAO)
+        {
+            _viecLamDAO = viecLamDAO;
+            _nguoiDungDAO = nguoiDungDAO;
+            _ungTuyenDAO = ungTuyenDAO;
+
+        }
+
+        //Quan Ly Viec Lam
+        [HttpGet]
+        [Route("Index")]
+        [Route("")]
+        public IActionResult QuanLyViecLam()
+        {
+            return View("~/Views/Admin/Index.cshtml");
+        }
+
+    }
 }
